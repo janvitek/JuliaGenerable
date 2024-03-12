24b6422700ec8b6e669b5f3773b029cc2c18a48a
Jan Ječmen <jan.jecmen@gmail.com>
2024-03-12 16:10:57 +0000

    Fix Colon not found in Core

--- Project/src/main/java/prlprg/App.java
+++ Project/src/main/java/prlprg/App.java
@@ -110,7 +110,7 @@ public class App {
                     "abstract type Core.Union end", // This is used when Union occurs without arguments
                     "abstract type Core.Vararg end", // Missing in discovery
                     "struct Core.DataType <: Core.Type{T} end", // Missing in discovery
-                    "struct Core.Colon <: Core.Function end" };// Missing in discovery
+                    "struct Base.Colon <: Core.Function end" };// Missing in discovery
 
             // Reading nanes only to initialize NameUtils                    
             new Parser().withFile(aliasesPath).lex().parseAliasNames();
